8eddce38c109d51be0f72b9df6566aceaecc4d00
jiaquan <jiaquan@staff.sina.com.cn>
2019-10-08 11:38:01 +0800

    upd

--- src/blocks/h5/index.js
+++ src/blocks/h5/index.js
@@ -1,5 +1,5 @@
 import Component from './Component.vue'
 if (window.Vue) {
-  Vue.component(require('../../../package.json').name, Component)
+  Vue.component(require('../schema.json').type, Component)
 }
 export default Component
